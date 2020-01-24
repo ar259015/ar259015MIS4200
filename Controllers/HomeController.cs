0312139c2d7431b33cceeee20ee17cd2b223b7f8
Abby Rachel <ar259015@ohio.edu>
2020-01-24 11:47:15 -0500

    Changes to Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace ar259015MIS4200.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My MIS4200 description.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "My Contact Information.";
 
             return View();
         }
